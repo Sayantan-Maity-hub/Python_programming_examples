7dd3e91b879a6ac5c5c3f960fdd4291a5cb1020f
Sayantan-maity-hub <23F3000182@es.study.iitm.in>
2025-03-20 11:04:16 +0530

    division added

---
+++ add_numbers.py
@@ -0,0 +1,7 @@
+# add two numbers
+a = int(input("Input first number:"))
+b = int(input("Input second number:"))
+print("the sum is:", a+b)
+print("The substruction is:", a-b)
+print("The multiplication is:", a*b)
+print("The division is:", a/b)
