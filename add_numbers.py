7e118887d8e9f4dead466b81d3e7e2675336ea99
Sayantan Maity <23f3000182@es.study.iitm.ac.in>
2025-03-20 11:14:50 +0530

    Just added new comment nothing else

--- add_numbers.py
+++ add_numbers.py
@@ -1,4 +1,4 @@
-# add two numbers
+# Additon, subtruction, multiplication, division of two given number.
 a = int(input("Input first number:"))
 b = int(input("Input second number:"))
 print("the sum is:", a+b)
