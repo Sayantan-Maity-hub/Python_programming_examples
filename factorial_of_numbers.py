275599a9af62059e4a9a2ad75a8c2c324785861d
Sayantan Maity <23f3000182@es.study.iitm.ac.in>
2025-03-20 13:08:29 +0530

    Add files via upload
    Signed-off-by: Sayantan Maity <23f3000182@es.study.iitm.ac.in>

---
+++ factorial_of_numbers.py
@@ -0,0 +1,9 @@
+a = int(input("enter a number:")) # for take input from user.
+factorial = 1 
+
+for i in range(1, a+1): # loop for multiple all numbers range 1 to a.
+    factorial *=i
+
+
+print("The factorial of a given number is:", factorial)
+
